3efa730da709a5950de7866ed034187765b10d43
jlalvarezfernandez <jl.alvarezfernandez@hotmail.com>
2021-12-15 13:02:56 +0100

    cambio de constante de api 'http://cpd.iesgrancapitan.org:9117'

--- fronted/proyecto/src/app/services/data.service.ts
+++ fronted/proyecto/src/app/services/data.service.ts
@@ -11,7 +11,9 @@ import { Test } from '../models/Test';
 export class DataService {
 
   // private api = 'http://127.0.0.1:8000/api';
-  private api = 'http://frontyincanas.iesgrancapitan.org/api';
+  // private api = 'http://frontyincanas.iesgrancapitan.org/api';
+  private api = 'http://cpd.iesgrancapitan.org:9117';
+
 
   constructor(
     private http: HttpClient,
